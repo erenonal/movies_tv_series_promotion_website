85d05fd477ecbab5867c3790c748ecf646b3d5cf
Eren Önal <75484033+erenonal@users.noreply.github.com>
2024-03-16 13:38:12 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Vitalen</div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+</style>
